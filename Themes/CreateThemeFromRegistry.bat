61c0b9ae7c96c6a5d653c1637de870714ef7eb83
Zazon <thediogopcm@gmail.com>
2023-09-14 00:39:45 +0100

    Improved theme tool to allow more GameMakers Added new themes for 8.1

--- Themes/CreateThemeFromRegistry.bat
+++ Themes/CreateThemeFromRegistry.bat
@@ -1,20 +1,30 @@
 @echo off
 echo %CD%
-echo WARNING: Close GameMaker 8.2 before running this
+echo WARNING: Close the GameMaker instance before running this
 echo Press Enter to proceed...
 pause > nul
 cls
 setlocal enabledelayedexpansion
 
 ::Registry key path
-set "RegKey=HKEY_CURRENT_USER\Software\Game Maker\Version 8.2\Preferences"
+set /p "ChosenVersion=Enter GameMaker version (7, 8, 8.1, 8.2...): "
+set "RegistryName=CodeColor"
+
+::Check if ChosenVersion is 7 and change to its right color
+if "%ChosenVersion%"=="7" (
+    set "RegistryName=Color"
+)
+
+set "RegKey=HKEY_CURRENT_USER\Software\Game Maker\Version %ChosenVersion%\Preferences"
 
 ::File path
-set "RegFile=MyTheme8.reg"
+::Remove dot, for example 8.1 to 81
+set "ChosenVersionNoDot=%ChosenVersion:.=%"
+set "RegFile=GM%ChosenVersionNoDot%_MyTheme.reg"
 
 ::Create an empty .reg file and write this to it
 echo Windows Registry Editor Version 5.00 > "%RegFile%"
-echo [HKEY_CURRENT_USER\Software\Game Maker\Version 8.2\Preferences]>> "%RegFile%"
+echo [%RegKey%]>> "%RegFile%"
 
 ::Whether the current line is a "CodeColorN"
 set IsColor=0
@@ -27,21 +37,21 @@ for /f "tokens=1,2*" %%A in ('reg query "%RegKey%"') do (
     set IsColor=0
 
     ::Check whether the current line is a CodeColor
-    if "!Name!" equ "CodeColor0" set IsColor=1
-    if "!Name!" equ "CodeColor1" set IsColor=1
-    if "!Name!" equ "CodeColor2" set IsColor=1
-    if "!Name!" equ "CodeColor3" set IsColor=1
-    if "!Name!" equ "CodeColor4" set IsColor=1
-    if "!Name!" equ "CodeColor5" set IsColor=1
-    if "!Name!" equ "CodeColor6" set IsColor=1
-    if "!Name!" equ "CodeColor7" set IsColor=1
-    if "!Name!" equ "CodeColor8" set IsColor=1
-    if "!Name!" equ "CodeColor9" set IsColor=1
-    if "!Name!" equ "CodeColor10" set IsColor=1
-    if "!Name!" equ "CodeColor11" set IsColor=1
+    if "!Name!" equ "%RegistryName%0" set IsColor=1
+    if "!Name!" equ "%RegistryName%1" set IsColor=1
+    if "!Name!" equ "%RegistryName%2" set IsColor=1
+    if "!Name!" equ "%RegistryName%3" set IsColor=1
+    if "!Name!" equ "%RegistryName%4" set IsColor=1
+    if "!Name!" equ "%RegistryName%5" set IsColor=1
+    if "!Name!" equ "%RegistryName%6" set IsColor=1
+    if "!Name!" equ "%RegistryName%7" set IsColor=1
+    if "!Name!" equ "%RegistryName%8" set IsColor=1
+    if "!Name!" equ "%RegistryName%9" set IsColor=1
+    if "!Name!" equ "%RegistryName%10" set IsColor=1
+    if "!Name!" equ "%RegistryName%11" set IsColor=1
 
     ::Write if this registry is named "CodeColorN"
-    if !IsColor! equ 1 (        
+    if !IsColor! equ 1 (
         set "ValueName=!ValueName:x=0!"
         echo "!Name!"=%Type%dword:!ValueName!>> "%RegFile%"
     )
